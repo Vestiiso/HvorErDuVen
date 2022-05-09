f2e430eae717821a3386a0b95971835259531e6b
Vestiiso <mailtilatloggeind@protonmail.com>
2022-05-10 00:05:23 +0200

    hello

--- app/src/main/java/com/example/hvorerduven/Hovedsiden.java
+++ app/src/main/java/com/example/hvorerduven/Hovedsiden.java
@@ -3,14 +3,23 @@ package com.example.hvorerduven;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Hovedsiden extends AppCompatActivity {
-
-    //tester 123456
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hovedsiden);
+
+        ArrayList<ExampleItem> exampleList = new ArrayList<>();
+        exampleList.add(new ExampleItem(R.drawable.ic_alarm, "linje 1", "linje 2"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "linje 1.1", "linje 2.2"));
+        exampleList.add(new ExampleItem(R.drawable.ic_baseline, "linje 1.2", "linje 2.2"));
     }
 }
