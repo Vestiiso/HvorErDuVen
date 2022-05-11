79b1b3d58714cc4037e5cbba655f9041b2f07de5
Vestiiso <mailtilatloggeind@protonmail.com>
2022-05-12 00:05:06 +0200

    har lavet buttons der åbner vores aktiviteter fra main, men der er psyko mange problemer.

--- app/src/main/java/com/example/hvorerduven/MainActivity.java
+++ app/src/main/java/com/example/hvorerduven/MainActivity.java
@@ -1,36 +1,26 @@
 package com.example.hvorerduven;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    //test opdaterer den?
-
-
-    private EditText number, message;
-    private Button send;
     private Button testHovedsiden;
+    private Button testSendSMS;
+    private Button testSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        number = findViewById(R.id.number);
-        message = findViewById(R.id.message);
-        send = findViewById(R.id.send);
         testHovedsiden = (Button) findViewById(R.id.testHovedsiden);
+        testSendSMS = (Button) findViewById(R.id.testSendSMS);
+        testSettings = (Button) findViewById(R.id.testSettings);
 
         testHovedsiden.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,24 +29,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        send.setOnClickListener(new View.OnClickListener() {
+        testSendSMS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-
-                    if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
-
-                        sendSMS();
-                    }
-                    else{
-                        requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
-
-                    }
-                }
-
+                openSendSMS();
             }
         });
+
+        testSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettings();
+            }
+        });
+
+
     }
 
     public void openHovedsiden() {
@@ -64,21 +51,15 @@ public class MainActivity extends AppCompatActivity {
         startActivity(hovedsidenIntent);
     }
 
-
-    private void sendSMS() {
-        String phoneNo = number.getText().toString().trim();
-        String SMS = message.getText().toString().trim();
-
-        try {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNo, null,SMS,null,null);
-            Toast.makeText(this, "SMS Sent!", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Failed to send SMS", Toast.LENGTH_SHORT).show();
-        }
-
-
+    public void openSendSMS () {
+        Intent SendSMSIntent = new Intent(this, SendSMS.class);
+        startActivity(SendSMSIntent);
     }
+
+    public void openSettings() {
+        Intent SettingsIntent = new Intent(this, Settings.class);
+        startActivity(SettingsIntent);
+    }
+
 }
 
--- app/src/main/java/com/example/hvorerduven/Settings.java
+++ app/src/main/java/com/example/hvorerduven/Settings.java
@@ -1,19 +1,16 @@
 package com.example.hvorerduven;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
 
 public class Settings extends AppCompatActivity {
 
     private Button Security;
+
+    private Button Logout;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
