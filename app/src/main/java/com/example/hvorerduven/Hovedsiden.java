e8d6e8cf085b2f2a860cfc4d432246067b599593
Vestiiso <mailtilatloggeind@protonmail.com>
2022-05-14 23:36:05 +0200

    begyndt at arbejde på at indsætte kort

--- app/src/main/java/com/example/hvorerduven/Hovedsiden.java
+++ app/src/main/java/com/example/hvorerduven/Hovedsiden.java
@@ -1,6 +1,8 @@
 package com.example.hvorerduven;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,38 +11,49 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class Hovedsiden extends AppCompatActivity {
+
+    private ArrayList<Card> mCardList;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private Button buttonInsert;
+    private Button buttonRemove;
+    private EditText editTextInsert;
+    private EditText editTextRemove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hovedsiden);
 
-        /*ArrayList<ExampleItem> exampleList = new ArrayList<>();
-        exampleList.add(new ExampleItem(R.drawable.ic_alarm, "linje 1", "linje 2"));
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "linje 1.1", "linje 2.1"));
-        exampleList.add(new ExampleItem(R.drawable.ic_baseline, "linje 1.2", "linje 2.2"));
+        lavCardList();
+        buildRecyclerView();
 
-         */
+        buttonInsert = findViewById(R.id.button_insert);
+        buttonRemove = findViewById(R.id.button_remove);
+        editTextInsert = findViewById(R.id.edittext_insert);
+        editTextRemove = findViewById(R.id.edittext_remove);
 
+    }
 
-        ArrayList<Card> cardList = new ArrayList<>();
-        cardList.add(new Card("Camp området"));
-        cardList.add(new Card("main stage"));
-        cardList.add(new Card("baren"));
+    public void lavCardList() {
+        mCardList = new ArrayList<>();
+        mCardList.add(new Card("Camp området"));
+        mCardList.add(new Card("main stage"));
+        mCardList.add(new Card("baren"));
+    }
 
-
+    public void buildRecyclerView() {
         //listen bliver givet til vores adapter, som giver den til vores viewholder
         mRecyclerView = findViewById(R.id.recyclerView);
         //mRecyclerView.setHasFixedSize(true); //hvis vi ved at listen ikke ændrer størrelse, kan denne linje optimere performance
         mLayoutManager = new LinearLayoutManager(this);
         //mAdapter = new CardAdapter(exampleList);
-        mAdapter = new CardAdapter(cardList);
+        mAdapter = new CardAdapter(mCardList);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
-
     }
+
 }
