c2752876aa6202006a0529a65ed8b309bbfbf904
MarcusStjerno <90749370+MarcusStjerno@users.noreply.github.com>
2022-05-21 14:04:45 +0200

    opret bruger knap virker, kan implementere firebase nu

--- app/src/main/java/com/example/hvorerduven/OpretBruger.java
+++ app/src/main/java/com/example/hvorerduven/OpretBruger.java
@@ -2,7 +2,11 @@ package com.example.hvorerduven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class OpretBruger extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class OpretBruger extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opret_bruger);
+
+
+        MaterialButton opretNyBruger = (MaterialButton) findViewById(R.id.opretNyBruger);
+        opretNyBruger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
