60e61f47287129b9a029bad53d3de4f4e99c0fab
Stjerno <Marcus@stjerno.dk>
2022-05-21 11:28:43 +0200

    opret bruger knap + getstring, men knap virker ikke?

--- app/src/main/java/com/example/hvorerduven/OpretBruger.java
+++ app/src/main/java/com/example/hvorerduven/OpretBruger.java
@@ -3,12 +3,33 @@ package com.example.hvorerduven;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class OpretBruger extends AppCompatActivity {
 
+    Button Opret;
+    EditText mEdit;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opret_bruger);
+
+        Opret = (Button)findViewById(R.id.opretTilFirebase);
+        mEdit   = (EditText)findViewById(R.id.username);
+
+        Opret.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Log.v("EditText", mEdit.getText().toString());
+                    }
+                });
     }
 }
