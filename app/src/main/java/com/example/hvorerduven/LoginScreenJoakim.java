0d28e437559dd7b2d108fdfa0b9accc41eb790e3
Stjerno <Marcus@stjerno.dk>
2022-05-23 14:50:05 +0200

    opret bruger virker + nickos kode

--- app/src/main/java/com/example/hvorerduven/LoginScreenJoakim.java
+++ app/src/main/java/com/example/hvorerduven/LoginScreenJoakim.java
@@ -2,8 +2,11 @@ package com.example.hvorerduven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,12 +18,23 @@ import com.google.firebase.storage.StorageReference;
 
 public class LoginScreenJoakim extends AppCompatActivity {
 
+    private Button opretBruger;
+
     private FirebaseStorage storage = FirebaseStorage.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen_joakim);
+
+        opretBruger = (Button) findViewById(R.id.opretbutton);
+
+        opretBruger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openOpretBruger();
+            }
+        });
         // Write a message to the database
 
         String username = "kristoffer";
@@ -80,4 +94,10 @@ public class LoginScreenJoakim extends AppCompatActivity {
             }
         });
     }
+
+    public void openOpretBruger() {
+        Intent OpretBrugerIntent = new Intent(this, OpretBruger.class);
+        startActivity(OpretBrugerIntent);
+    }
+
 }
--- app/src/main/java/com/example/hvorerduven/OpretBruger.java
+++ app/src/main/java/com/example/hvorerduven/OpretBruger.java
@@ -1,14 +1,51 @@
 package com.example.hvorerduven;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hvorerduven.R;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import com.google.android.material.button.MaterialButton;
+
+
 
 public class OpretBruger extends AppCompatActivity {
 
+    private Button brugernavn;
+    EditText editBruger;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opret_bruger);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_opret_bruger);
+
+
+            MaterialButton opretNyBruger = (MaterialButton) findViewById(R.id.opretNyBruger);
+            opretNyBruger.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+            brugernavn = (Button)findViewById(R.id.opretNyBruger);
+            editBruger   = (EditText)findViewById(R.id.username);
+
+            brugernavn.setOnClickListener(
+                    new View.OnClickListener()
+                    {
+                        public void onClick(View view)
+                        {
+                            Log.v("TEST123", editBruger.getText().toString());
+                        }
+                    });
+        }
     }
-}
+
